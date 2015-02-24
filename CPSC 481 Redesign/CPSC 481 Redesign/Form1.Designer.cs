4f6d3fdc10f731aaa0de3520aceae9c5f53d7b7f
ahrmond <ahrmondji@gmail.com>
2015-02-24 15:04:24 -0700

    Quit Button

--- CPSC 481 Redesign/CPSC 481 Redesign/Form1.Designer.cs
+++ CPSC 481 Redesign/CPSC 481 Redesign/Form1.Designer.cs
@@ -46,11 +46,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(717, 66);
+            this.button2.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(711, 22);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 39);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "QUIT";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
