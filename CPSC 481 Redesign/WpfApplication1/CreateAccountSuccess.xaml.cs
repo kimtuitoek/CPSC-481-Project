fd9e617c384864286efed6bc0338ca0f759ac602
ahrmond <ahrmondji@gmail.com>
2015-03-30 12:32:16 -0600

    back to normal

--- CPSC 481 Redesign/WpfApplication1/CreateAccountSuccess.xaml.cs
+++ CPSC 481 Redesign/WpfApplication1/CreateAccountSuccess.xaml.cs
@@ -17,9 +17,9 @@ namespace WpfApplication1
     /// <summary>
     /// Interaction logic for Window2.xaml
     /// </summary>
-    public partial class Window2 : Window
+    public partial class CreateAccountSuccess : Window
     {
-        public Window2()
+        public CreateAccountSuccess()
         {
             InitializeComponent();
         }
--- CPSC 481 Redesign/WpfApplication1/Groups.xaml.cs
+++ CPSC 481 Redesign/WpfApplication1/Groups.xaml.cs
@@ -11,9 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Markup;
-using System.IO;
-using System.Xml;
 
 namespace WpfApplication1
 {
@@ -31,112 +28,6 @@ namespace WpfApplication1
         {
             Home.friends.Show();
             this.Hide();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void LeaveGroup(object sender, RoutedEventArgs e)
-        {
-            // go through the YourGroups list, remove all that are checked
-            int i = 0;
-
-            while (YourGroups.HasItems)
-            {
-                try
-                {
-                    Object obj = YourGroups.Items[i++];
-
-                    if (obj != null)
-                    {
-                        // get selected object
-                        StackPanel sp = (StackPanel)obj;
-
-                        // check if checkbox is selected
-                        CheckBox cb = (CheckBox)sp.Children[4];
-                        if (!(bool)cb.IsChecked)
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            // remove group
-                            YourGroups.Items.Remove(obj);
-
-                        }
-                    }
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    break;
-                }
-            }
-        }
-
-        private void JoinGroup(object sender, RoutedEventArgs e)
-        {
-            // go through Other Groups list, add all that are checked
-            int i = 0;
-            while (OtherGroups.HasItems)
-            {
-                try {
-                    Object obj = OtherGroups.Items[i++];
-                    if (obj != null) {
-                        // get selected object
-                        StackPanel sp = (StackPanel)obj;
-
-                         // check if checkbox is selected
-                        CheckBox cb = (CheckBox)sp.Children[4];
-                        if (!(bool)cb.IsChecked) {
-                            continue;
-                        }
-
-                         // check if this group has already been added
-                        bool added = false;
-                        TextBlock tb = (TextBlock)sp.Children[2];
-                        int j = 0;
-                        while (YourGroups.HasItems) {
-                            try {
-                                Object obje = YourGroups.Items[j++];
-                                StackPanel spa = (StackPanel)obje;
-                                TextBlock tbl = (TextBlock)spa.Children[2];
-
-                                if (tb.Text.Equals(tbl.Text)) {
-                                    added = true;
-                                    break;
-                                }
-                            }
-                            catch (ArgumentOutOfRangeException) {
-                                break;
-                            }
-                        }
-
-                        // otherwise, add friend
-                        if (!added)
-                        {
-                            // create a deep copy (clone)
-                            // source: https://msdn.microsoft.com/en-us/library/system.windows.markup.xamlwriter%28v=vs.110%29.aspx
-                            String savedStackPanel = XamlWriter.Save(sp);
-                            StringReader sr = new StringReader(savedStackPanel);
-                            XmlReader xr = XmlReader.Create(sr);
-                            StackPanel sp1 = (StackPanel)XamlReader.Load(xr);
-
-                            // cloned stackpanel should not have checkbox selected
-                            cb = (CheckBox)sp1.Children[4];
-                            cb.IsChecked = false;
-
-                            // add item to the friend list
-                            YourGroups.Items.Add(sp1);
-                        }
-                    }
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    break;
-                }
-            }
         }
     }
 }
